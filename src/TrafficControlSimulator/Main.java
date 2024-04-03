167831f40bb3f5861f9c08d62f8e3cca45b174fd
kerema14 <kerem.adali14@gmail.com>
2024-04-03 14:38:58 +0300

    settings.json updated

--- src/TrafficControlSimulator/Main.java
+++ src/TrafficControlSimulator/Main.java
@@ -7,23 +7,33 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
-
+import javafx.scene.control.Button;
+import javafx.scene.layout.GridPane;
+import javafx.scene.layout.StackPane;
+import javafx.scene.shape.Rectangle;
+import javafx.scene.text.Text;
 import javafx.stage.Stage;
 
 public class Main extends Application {
     @Override // Override the start method in the Application class
     public void start(Stage primaryStage) throws IOException {
-        /*  Create a button and place it in the scene
-        Button btOK = new Button("OK");
-        Scene scene = new Scene(btOK, 200, 250);
-        primaryStage.setTitle("MyJavaFX"); // Set the stage title
+        
+        Button selectFileButton = new Button("Select level file");
+        selectFileButton.setOnMouseClicked(event -> {
+            
+        });
+        Text headerText = new Text("Traffic Control Simulator");
+        GridPane gridPane = new GridPane();
+        gridPane.add(selectFileButton, 800/2, 800/2);
+        gridPane.add(headerText, 800/2, 800/2 - 50);
+        gridPane.setHgap(1);
+        gridPane.setVgap(1);
+        Scene scene = new Scene(gridPane, 800, 800);
+        primaryStage.setTitle("Traffic Control Simulator"); // Set the stage title
         primaryStage.setScene(scene); // Place the scene in the stage
-        primaryStage.show();  Display the stage*/
-        Parent root = FXMLLoader.load(getClass().getResource("hellofx.fxml"));
-        primaryStage.setTitle("Hello World");
-        primaryStage.setScene(new Scene(root, 400, 300));
         primaryStage.show();
     }
+    
     public static void main(String[] args) {
         launch(args);
     }
