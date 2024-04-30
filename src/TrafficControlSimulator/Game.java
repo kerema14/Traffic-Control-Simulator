fe3b3fde083d2cc6c91e4abd525efaaef4d19b17
bugra <bugrakaya020@gmail.com>
2024-04-30 14:15:49 +0300

    max car in the scene implemented

--- src/TrafficControlSimulator/Game.java
+++ src/TrafficControlSimulator/Game.java
@@ -16,120 +16,137 @@ import javafx.scene.text.Font;
 import javafx.scene.text.Text;
 
 public class Game {
-    
-    
-    private LevelParser levelParser;
-    private LevelPane levelPane;
+
+	private LevelParser levelParser;
+	private LevelPane levelPane;
+	
 	public ArrayList<Building> buildings = new ArrayList<>();
 	public ArrayList<RoadTile> roadTiles = new ArrayList<>();
 	public ArrayList<TrafficLight> trafficLights = new ArrayList<>();
-    public ArrayList<Path> paths = new ArrayList<>();
-    public static ArrayList<Timeline> animationTimelines;
-    static Timeline carTimeline; 
-    public long oldNow;
-    public double millisecondsPassed;
-    public double totalMillisecondsPassed;
-    public double spawnTimeStamp;
-    public ArrayList<Vehicle> cars = new ArrayList<>();
+	public ArrayList<Path> paths = new ArrayList<>();
+	public ArrayList<Vehicle> cars = new ArrayList<>();
+	
+	public static ArrayList<Timeline> animationTimelines;
+	static Timeline carTimeline;
+	
+	public int spawnedCarNum;
+	public int reachedCarNum;
+	public int accidentNum;
+	public long oldNow;
+	public double millisecondsPassed;
+	public double totalMillisecondsPassed;
+	public double spawnTimeStamp;
 
-    public void initData(LevelPane levelPane){
-        
-        animationTimelines = new ArrayList<Timeline>();
-        levelParser = levelPane.getLevelParser();
-        this.levelPane = levelPane;
-        this.trafficLights = levelParser.trafficLights;
-        paths = levelParser.paths;
-        for(Path path:paths){
-            path.setOpacity(0.0);
-            levelPane.getChildren().add(path);
-        }
-        createTraffic();
+	public void initData(LevelPane levelPane) {
+		spawnedCarNum = 0;
+		reachedCarNum = 0;
+		accidentNum = 0;
 
-    }
-    public void createTraffic() {
-        
-        AnimationTimer timer = new AnimationTimer() { 
-            @Override 
-            public void handle(long now) { 
-                update(now); 
-            } 
-        }; 
-        timer.start(); 
-    }
-    public void update(long now){
+		animationTimelines = new ArrayList<Timeline>();
+		
+		levelParser = levelPane.getLevelParser();
+		
+		this.levelPane = levelPane;
+		
+		this.trafficLights = levelParser.trafficLights;
+		paths = levelParser.paths;
+		for (Path path : paths) {
+			path.setOpacity(0.0);
+			levelPane.getChildren().add(path);
+		}
+		
+		createTraffic();
+	}
+
+	public void createTraffic() {
+
+		AnimationTimer timer = new AnimationTimer() {
+			@Override
+			public void handle(long now) {
+				update(now);
+			}
+		};
+		timer.start();
+	}
+
+	public void update(long now){
         totalMillisecondsPassed = now/1000000.0;
         millisecondsPassed = (now-oldNow)/1000000.0;
         
-        if (totalMillisecondsPassed-spawnTimeStamp > 600&&!levelPane.isGameOver) {
+        int currentCarNum = spawnedCarNum - (reachedCarNum + accidentNum);
+        
+        if (totalMillisecondsPassed-spawnTimeStamp > 600 && !levelPane.isGameOver && currentCarNum < 10) {
             spawnCar();
             spawnTimeStamp = totalMillisecondsPassed;
             
         }
         carBehaviour();
         
-        
-        
         oldNow = now;
     }
-    public void spawnCar(){
-        Random random = new Random();
-        
-        Path carPath = paths.get(random.nextInt(paths.size()));
-        
-        
-        Vehicle v = new Vehicle(carPath);
-        
-        
-        cars.add(v);
 
-        
-        v.setTranslateX(v.paneX);
-        v.setTranslateY(v.paneY);
-        levelPane.getChildren().add(v);
-        v.startPathTransition();
-        
-        
-    }
-    public void carBehaviour(){
-    	try {
-        for(Vehicle car : cars){
-            if (car.isCollidible() == false) {
-            	if(car.wrecked)
-            		levelParser.increasCarAccident();
-            	else 
-            		levelParser.increasReachCars();
-                levelPane.getChildren().remove(car);
-                cars.remove(car);
-                continue;
-            }
-            car.checkAhead(trafficLights, cars);
-            levelPane.updateScoreText();
-            levelPane.updateAccidentText();
-        }
-    	}catch(Exception e){}
-        if(levelParser.getCarAccident()/2>=levelParser.getMaxCarAccident()||levelParser.getReachCars()>=levelParser.getCarNumToWin()) {
-        	//Pause to whole game 
-        	for(Vehicle car :cars) {
-        		car.pausePathTransition();
-        	}
-        	//Lose / Win Text
-        	Text loseWinText = new Text();
-    		loseWinText.setLayoutX(300);
-    		loseWinText.setLayoutY(100);
-    		loseWinText.setFont(Font.font("Arial",45));
-        	if(levelParser.getCarAccident()/2>=levelParser.getMaxCarAccident()) {
-        		loseWinText.setText("You Lose");
-        		levelPane.isGameOver = true;
-        	}else {
-        		loseWinText.setText("You Win");
-        		levelPane.isGameOver = true;
-        	}
-        	levelPane.getChildren().add(loseWinText);
-        	
-        }
+	public void spawnCar() {
+		Random random = new Random();
 
-    }
-    
-    
-    
+		Path carPath = paths.get(random.nextInt(paths.size()));
+
+		Vehicle v = new Vehicle(carPath);
+
+		cars.add(v);
+
+		v.setTranslateX(v.paneX);
+		v.setTranslateY(v.paneY);
+		levelPane.getChildren().add(v);
+		v.startPathTransition();
+		
+		spawnedCarNum++;
+	}
+
+	public void carBehaviour() {
+		try {
+			for (Vehicle car : cars) {
+				if (car.isCollidible() == false) {
+					if (car.wrecked) {
+						levelParser.increasCarAccident();
+						accidentNum++;
+					}else {
+						levelParser.increasReachCars();
+						reachedCarNum++;
+					}
+					
+					levelPane.getChildren().remove(car);
+					cars.remove(car);
+					
+					continue;
+				}
+				car.checkAhead(trafficLights, cars);
+				levelPane.updateScoreText();
+				levelPane.updateAccidentText();
+			}
+		} catch (Exception e) {
+		}
+		if (levelParser.getCarAccident() / 2 >= levelParser.getMaxCarAccident()
+				|| levelParser.getReachCars() >= levelParser.getCarNumToWin()) {
+			// Pause to whole game
+			for (Vehicle car : cars) {
+				car.pausePathTransition();
+			}
+			// Lose / Win Text
+			Text loseWinText = new Text();
+			loseWinText.setLayoutX(300);
+			loseWinText.setLayoutY(100);
+			loseWinText.setFont(Font.font("Arial", 45));
+			if (levelParser.getCarAccident() / 2 >= levelParser.getMaxCarAccident()) {
+				loseWinText.setText("You Lose");
+				levelPane.isGameOver = true;
+			} else {
+				loseWinText.setText("You Win");
+				levelPane.isGameOver = true;
+			}
+			levelPane.getChildren().add(loseWinText);
+
+		}
+
+	}
+
 }
